725d76f1360da7cdd259ce4e4f5c3b42c72dab7c
chaeeun <btd.chaeeun@gmail.com>
2021-02-01 01:38:25 +0900

    props 기초

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
 import React from 'react';
 
-function Food({ fav }){
-  return <h1>I like { fav }!</h1>;
+function Food({ name, picture }){
+  return (
+    <div>
+        <h2>I like {name}!</h2>
+        <img src={picture} />
+    </div>
+  );
 }
+
+const foodILinke = [
+  {
+    name: '나나',
+    image: 'https://image-notepet.akamaized.net/resize/620x-/seimage/20200722%2Fc78376a631ffd4c6683e28036e01c133.jpg',
+  },
+  {
+    name: '코코',
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7eE_O_Y93Ed6pQPS-fzAZpR08Yh69KaC03Q&usqp=CAU',
+  },
+  {
+    name: '체리',
+    image: 'https://imgc.1300k.com/aaaaaib/goods/215024/68/215024683959.jpg?3',
+  }
+];
 
 function App() {
   return (
       <div>
-        <h1>Hello!!!</h1>
-        <Food fav="tomato" />
-        <Food fav="ramen" />
-        <Food fav="samgiopsal" />
-        <Food fav="chukumi" />
+        {foodILinke.map(dish => (<Food name={dish.name} picture={dish.image} />))}
       </div> 
     );
 }
